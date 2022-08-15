4f6c260f768ccb1a54659c375e291343c60f5614
sherry <sherry.kisilu@studebnt.moringaschool.com>
2022-08-15 23:48:31 +0300

    assignment compelete

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+//console.log(destructivelyAppendCat("Ralph"));
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+// console.log(destructivelyPrependCat("Bob"))
+function destructivelyRemoveLastCat(name){
+    return cats.pop(name);
+}
+// console.log (destructivelyRemoveLastCat("Garfield"))
+function destructivelyRemoveFirstCat(name){
+    return cats.shift(name);
+}
+function appendCat(name){
+    let copyCat=[...cats,name]
+    return copyCat;
+}
+function prependCat(name){
+    let anotherCat=[name,...cats]
+    return anotherCat;
+}
+function removeLastCat(){
+    let newArray=cats.slice(0,-1);
+    console.log(newArray);
+    return newArray;
+}
+removeLastCat();
+function removeFirstCat(){
+    let another=cats.slice(1);
+    return another;
+}
+console.log(cats.length);
+
+
